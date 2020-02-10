394ce75ae7b86e392d29929f6a263481071c382d
Luc Frébourg <l.frebourg@gmail.com>
2020-02-10 11:25:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
-//var watson = require('ibm-watson');
 var mysql  = require('mysql');
 const AssistantV2 = require('ibm-watson/assistant/v2');
-const { BasicAuthenticator } = require('ibm-watson/auth');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 
 //à remplacer **************************************
-var watsonSkillId = "42e1a173-5cb4-4a7a-896f-f62b97452960";
 var watsonApiKey = "";
-// s'arreter avant /v1
-var watsonLegacyv1WorkspaceURL ="https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/ba3e7f27-4e83-4906-86eb-2e206f355592";
+var watsonURL ="https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/ba3e7f27-4e83-4906-86eb-2e206f355592";
+var watsonID='1541c493-2a0e-4014-9c08-c27744e4aa08';
 
 var myRDSHost ="database-2.ctdswicaffyc.eu-west-3.rds.amazonaws.com";
 var myRDSLogin ="admin";
 var myRDSPassword ="";
-var myassistantid='ccf70d26-808d-45f2-95b8-7ba16ae8fee1';
-var myFacebookToken ="";
 
-//https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/ba3e7f27-4e83-4906-86eb-2e206f355592/v2/assistants/1541c493-2a0e-4014-9c08-c27744e4aa08/sessions/551371a1-2e29-473b-b2a4-57130984924c/message?version=2019-02-28
+var myFacebookToken ="";
 
 
 
@@ -31,36 +27,23 @@ var myUrl = process.env.assistant_URL;*/
 var app = express();
 
 
-
-// Assistant Watson V2
+//assistant v2
 const assistant = new AssistantV2({
   version: '2019-02-28',
-  authenticator: new BasicAuthenticator({
-    username: 'apikey',
-    password: watsonApiKey,
+  authenticator: new IamAuthenticator({
+    apikey: watsonApiKey,
   }),
-  url: watsonLegacyv1WorkspaceURL,
+  url: watsonURL,
 });
 
-// Assistant Watson V1
-/*var assistant = new watson.AssistantV1({
-    version :"2018-10-12",
-    username: "apikey",
-    password: watsonApiKey,
-    url: watsonLegacyv1WorkspaceURL,
-    
-});*/
+
 var sessionId="";
 assistant.createSession({
-  assistantId: 'ccf70d26-808d-45f2-95b8-7ba16ae8fee1' })  .then(res => {
-    console.log(JSON.stringify(res, null, 2));
+  assistantId: '1541c493-2a0e-4014-9c08-c27744e4aa08' })  .then(res => {
+    
     sessionId=res.result.session_id;
-    console.log(sessionId);
+    console.log("session id :"+ sessionId);
   });
-
-
-
-var contexid = "";
 
 
 // Mysql - MariaDB
@@ -73,7 +56,7 @@ var db = mysql.createConnection({
     user: myRDSLogin,
     password: myRDSPassword,
     database: "poj",
-    debug: true
+    debug: false
   });
 
 
@@ -93,7 +76,7 @@ app.get('/webhook/', function (req, res) {
 //appel de Watson Assistant et insertion de la reservation dans Mysql. Puis appel de sendMessage pour envoie vers Messenger
 function callWatson(payload, sender) {
     assistant.message(payload, function(err, data) {
-        console.log(data)
+        console.log(JSON.stringify(data.result.output))
         if (err)
             return console.log('error:', err);
         
@@ -213,22 +196,26 @@ app.post('/webhook/', function (req, res) {
 
 /* CLIENT HTML **************************************************************************************/
 function callWatsonClient(payload,res) {
-    console.log(payload);
+    console.log("\x1b[32m%s\x1b[0m",JSON.stringify(payload));
     assistant.message(payload, function(err, data) {
-        console.log(data)
+        console.log("\x1b[44m%s\x1b[0m" ,JSON.stringify(data.result.output));
         if (err)
             return console.log('error:', err);
         
-        if(data.context != null){
+
+        mesVariables=data.result.context.skills['main skill'].user_defined;
+
+
+        /*if(data.context != null){
             contexid = data.context;
-        }
+        }*/
         //reservation
-        if(data.context.confirmation !=null){
+        if(mesVariables.confirmation !=null){
                 
         //variables de context du chatbot
-                var date_reservation= data.context.date_reservation;
-                var nb_chambres= data.context.nb_chambre;
-                var room_category= data.context.room_category;
+                var date_reservation= mesVariables.date_reservation;
+                var nb_chambres= mesVariables.nb_chambre;
+                var room_category= mesVariables.room_category;
         //requete d'insertion
                var query='insert into reservation (idC,nbChambres,dateDebut,nbJour,categorie) values(null,'+nb_chambres+',"'+date_reservation+'",'+"null"+',"'+room_category+'")';
                 db.query(query, function (error, results, fields) {
@@ -244,45 +231,63 @@ function callWatsonClient(payload,res) {
 }
 
 app.post('/message', function (req, res) {
-       
-     var payload = {
-       assistantId: myassistantid, 
-       input: req.body.input,
-       context: req.body.context || {},
-       sessionId: sessionId,
-      
-     }
+    
+     var payload ={
+    assistantId: watsonID,
+    sessionId: sessionId,
+    input: {
+      message_type: 'text',
+      text: req.body.input.text,
+      options: {
+        'return_context': true
+    }
+    },
+    context:  {
+      'global': {
+        'system': {
+          'user_id': 'my_user_id'
+            }
+      },
+        'skills': {
+        'main skill': {
+          'user_defined': {
+            'option': null
+             }
+        }
+      }
+    }
+  };
+
+  if(req.body.context !="")
+  	payload.context=req.body.context;
+
+mesVariables=payload.context.skills['main skill'].user_defined;
 
 
 
 
-
-
-
-        if(payload.context.option !=null){
+        if(mesVariables.option !=null){
           //recherche dans la base le prix de l'option
-                 var query='select prix from option where libelle="'+payload.context.option+'")';
+                 var query='select prix from option where libelle="'+mesVariables.option+'")';
                  db.query(query, function (error, results, fields) {
                  if (error) {
                   console.log(JSON.stringify(error));
                  }
-            payload.sessionId.prixoption = results;
+                mesVariables.prixoption = results;
                 });
         }   
             
         //nombre de chambre défini 
-        if(payload.context.nb_chambre !=null){
+        if(mesVariables.nb_chambre !=null){
                     //montant de la chambre envoyé au chatbot
-                    payload.sessionId.total=100;
+                    mesVariables.total=100;
         }
     callWatsonClient(payload,res);
     
 
 });
-app.get('/message', function (req, res) {
-       
-     console.log("bvoovovo");
-    
+app.get('/message', function (req, res) {       
+     console.log("Il faut une methode POST");   
 
 });
 var path = require('path');
@@ -292,22 +297,11 @@ app.get('/', function (req, res) {
 /* CLIENT HTML END **************************************************************************************/
 
 
-console.log("ok");
+console.log("host: "+host+ "port: "+port);
 app.listen(port, host);
 
 
 
-//Pour tester un appel simple de Watson Assistant
-/*assistant.message({
-  workspace_id: myWorkspace,
-  input: {'text': 'reservation'}
-},  function(err, response) {
-  if (err)
-    console.log('error:', err);
-  else
-    console.log(JSON.stringify(response, null, 2));
-});
-*/
 //Pour tester Mysql
 /*
 db.query('SELECT * from reservation', function (error, results, fields) {
